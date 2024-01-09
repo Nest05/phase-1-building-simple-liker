66ffa982919e95582dd1ae2cf582885d7fb11322
Nestor Masinde <nestormasinde@gmail.com>
2024-01-09 07:43:31 +0300

    Building Simple Liker

--- main.js
+++ main.js
@@ -2,10 +2,38 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
+
 // Your JavaScript code goes here!
+const likeGlyphs = document.querySelectorAll('.like-glyph')
+const modal = document.querySelector('#modal')
+const modalMessage = document.querySelector('#modal-message')
+const serverCall = mimicServerCall();
 
-
-
+likeGlyphs.forEach(likeGlyph =>{
+likeGlyph.addEventListener('click', () => {
+  if (likeGlyph.innerHTML === EMPTY_HEART) {
+    likeGlyph.innerHTML = FULL_HEART
+    likeGlyph.classList.add('activated-heart')
+    serverCall
+   .then(res => {
+    likeGlyph.innerHTML = FULL_HEART
+    likeGlyph.classList.add('activated-heart')
+   })
+   .catch(error => {
+    
+    modalMessage.textContent = error;
+    modal.classList.remove('hidden');
+    setTimeout(() => modal.classList.add('hidden'), 3000)
+  })
+ }else{
+    likeGlyph.innerHTML = EMPTY_HEART
+    likeGlyph.classList.remove('activated-heart')
+ }
+  
+  
+  
+})
+})
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
